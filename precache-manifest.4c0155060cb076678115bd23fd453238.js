9447b7f600c015d022a3b661a3c6a73c65463b1b
Cristian Marquez <xthecapx@gmail.com>
2020-09-12 14:12:22 -0400

    Updates

---
+++ precache-manifest.4c0155060cb076678115bd23fd453238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960e077c817b86ea20b394c6b8698f3b",
+    "url": "/github/index.html"
+  },
+  {
+    "revision": "22b5275415e0bf32abb0",
+    "url": "/github/static/css/main.41d225b8.chunk.css"
+  },
+  {
+    "revision": "01ab44f43f15506cc811",
+    "url": "/github/static/js/2.8254fda2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/github/static/js/2.8254fda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b5275415e0bf32abb0",
+    "url": "/github/static/js/main.1e59d3f7.chunk.js"
+  },
+  {
+    "revision": "a00c0efc0682e0770ae6",
+    "url": "/github/static/js/runtime-main.8e5f0cc2.js"
+  }
+]);
